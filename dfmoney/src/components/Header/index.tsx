a978eb6de3b5290aa74efb18b657ee3995f7f72a
danielrgb2 <danielrgb2@gmail.com>
2021-12-22 19:54:24 -0300

    Componente(NewTransactionModal)

--- dfmoney/src/components/Header/index.tsx
+++ dfmoney/src/components/Header/index.tsx
@@ -2,12 +2,17 @@ import { Container, Content } from './styles';
 
 import logoImg from '../../assets/logo.svg';
 
-export function Header() {
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal}: HeaderProps ) {
+
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="df money" />
-                <button type="button">
+                <button type="button" onClick={onOpenNewTransactionModal}>
                     Nova transação
                 </button>
             </Content>
